0d8ab7078b9f53522fb5897f10096de86fb43f6a
YonghaoXu <yonghaoxu.whu@gmail.com>
2019-01-18 09:32:32 +0800

    Add files via upload

---
+++ model/SEAN.py
@@ -0,0 +1,66 @@
+import numpy as np
+import torch
+from torch import nn
+from torchvision import models
+
+class Classifier_Module(nn.Module):
+
+    def __init__(self, dims_in, dilation_series, padding_series, num_classes):
+        super(Classifier_Module, self).__init__()
+        self.conv2d_list = nn.ModuleList()
+        for dilation, padding in zip(dilation_series, padding_series):
+            self.conv2d_list.append(nn.Conv2d(dims_in, num_classes, kernel_size=3, stride=1, padding=padding, dilation=dilation, bias = True))
+
+        for m in self.conv2d_list:
+            m.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.conv2d_list[0](x)
+        for i in range(len(self.conv2d_list)-1):
+            out += self.conv2d_list[i+1](x)
+            return out
+
+
+class SEANet(nn.Module):
+    def __init__(self, num_classes, vgg16_caffe_path=None, pretrained=False):
+        super(SEANet, self).__init__()
+        vgg = models.vgg16()
+        if pretrained:
+            vgg.load_state_dict(torch.load(vgg16_caffe_path))
+
+        features, classifier = list(vgg.features.children()), list(vgg.classifier.children())
+
+        features = nn.Sequential(*(features[i] for i in list(range(23))+list(range(24,30))))
+
+        for i in [23,25,27]:
+            features[i].dilation = (2,2)
+            features[i].padding = (2,2)
+
+        fc6 = nn.Conv2d(512, 1024, kernel_size=3, padding=4, dilation=4)
+        fc7 = nn.Conv2d(1024, 1024, kernel_size=3, padding=4, dilation=4)
+
+        self.features = nn.Sequential(*([features[i] for i in range(len(features))] + [ fc6, nn.ReLU(inplace=True), fc7, nn.ReLU(inplace=True)]))
+
+        
+        #attention moduel
+        self.avgpool = nn.AvgPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True)
+        self.aconv = nn.Conv2d(1024, 1, kernel_size=1, stride=1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+        self.classifier = Classifier_Module(1024, [6,12,18,24],[6,12,18,24],num_classes)
+
+
+    def forward(self, x):
+        x = self.features(x)
+        
+        mask = self.avgpool(x)
+        interpolation = nn.UpsamplingBilinear2d(size=x.shape[2:4])
+        mask = interpolation(mask)
+        mask = self.aconv(mask)
+        mask = self.sigmoid(mask)
+
+        x = self.classifier((1+mask)*x)
+        return mask,x
+
+    def optim_parameters(self, args):
+        return self.parameters()
